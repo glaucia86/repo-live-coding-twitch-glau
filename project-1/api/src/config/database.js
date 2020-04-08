df66f94ea5669109ce8416b3f40b364f3769654c
glaucia86 <glaucia_lemos86@hotmail.com>
2020-04-07 22:50:14 -0300

    Desenvolvimento da conexão de dados com o node-postgres

---
+++ project-1/api/src/config/database.js
@@ -0,0 +1,27 @@
+/**
+ * Arquivo: config/database.js
+ * Descrição: arquivo responsável pelas 'connectionStrings da aplicação: PostgreSQL.
+ * Data: 15/04/2020
+ * Author: Glaucia Lemos
+ */
+
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// ==> Conexão com a Base de Dados (PostgreSQL)
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on('connect', () => {
+  console.log('Base de Dados conectado com sucesso!');
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text.params)
+};
+
+
+
